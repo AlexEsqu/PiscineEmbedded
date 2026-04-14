d966a00274e916c1682e7a4490a8c7b90633a901
Alex Kling <m2kling@gmail.com>
2026-04-14 09:39:58 +0200

    fix: removed variable define

--- d00/ex04/main.c
+++ d00/ex04/main.c
@@ -6,7 +6,7 @@
 /*   By: mkling <mkling@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/13 21:47:52 by mkling            #+#    #+#             */
-/*   Updated: 2026/04/14 09:33:00 by mkling           ###   ########.fr       */
+/*   Updated: 2026/04/14 09:38:40 by mkling           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,6 @@
 //  ◦ decrements a value each time you press button SW2
 //  ◦ displays this value in binary on LEDs D1 D2 D3 and D4 permanently.
 // You must use only the AVR registers ( DDRX, PORTX, PINX )
-
-#define SW1 (PIND & (PIND << PD2))
-#define SW2 (PIND & (PIND << PD4))
 
 void displayNumber(int num)
 {
@@ -40,6 +37,11 @@ void displayNumber(int num)
     PORTB |= binary1 | binary2 | binary4 | binary8;
 }
 
+bool isBeingPressed(int buttonPort, int buttonAddress)
+{
+    return ((buttonPort & (buttonPort << buttonAddress)) == 0);
+}
+
 
 int main()
 {
@@ -56,40 +58,40 @@ int main()
     PORTD |= (1 << 2) | (1 << 4);
 
     // Initializing the buttons to their current states
-    unsigned char sw1_is_pressed = (SW1 == 0);
-    unsigned char sw2_is_pressed = (SW2 == 0);
+    unsigned char sw1_is_pressed = isBeingPressed(PIND, PD2);
+    unsigned char sw2_is_pressed = isBeingPressed(PIND, PD4);
     unsigned char count = 0;
 
     while (1)
     {
-        if (!(SW1 == 0) && !(SW2 == 0))
+        if (!isBeingPressed(PIND, PD2) && !isBeingPressed(PIND, PD4))
             continue;
 
-        if (!sw1_is_pressed && (SW1 == 0))
+        if (!sw1_is_pressed && isBeingPressed(PIND, PD2))
         {
             count++;
             sw1_is_pressed = true;
-            while ((SW1 == 0))
+            while (isBeingPressed(PIND, PD2))
                 _delay_ms(30);
         }
-        if (sw1_is_pressed && !(SW1 == 0))
+        if (sw1_is_pressed && !isBeingPressed(PIND, PD2))
         {
             sw1_is_pressed = false;
-            while ((SW1 == 0))
+            while (isBeingPressed(PIND, PD2))
                 _delay_ms(30);
         }
 
-        if (!sw2_is_pressed && (SW2 == 0))
+        if (!sw2_is_pressed && isBeingPressed(PIND, PD4))
         {
             count--;
             sw2_is_pressed = true;
-            while ((SW2 == 0))
+            while (isBeingPressed(PIND, PD4))
                 _delay_ms(30);
         }
-        if (sw2_is_pressed && !(SW2 == 0))
+        if (sw2_is_pressed && !isBeingPressed(PIND, PD4))
         {
             sw2_is_pressed = false;
-            while ((SW2 == 0))
+            while (isBeingPressed(PIND, PD4))
                 _delay_ms(30);
         }
 
