4386aab79f7f8cc48f2b609eeddba417e222e543
Alex Kling <m2kling@gmail.com>
2026-04-16 11:00:15 +0200

    feat: d02 ex00 done!

--- Module02/ex00/main.c
+++ Module02/ex00/main.c
@@ -1,6 +1,8 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#include <utils/delay.h>
+#include <util/delay.h>
+
+# define BAUDRATE 115200
 
 // • The AVR ATmega328P microcontroller has 1 UART device that you must use in
 // 	 this exercise to communicate with a computer.
@@ -17,54 +19,76 @@
 // Initializing the USART, per 20.5 Initialization p. 184 - 185
 // Using 115200 as Baudrate
 
-// UCSRnA - USART Control and Status Register n A
+// UCSR0A - USART Control and Status Register n A
 // 	RXCn		TXCn		UDREn		FEn			DORn		UPEn		U2Xn		MPCMn
-//	0			0			1			0			0			0		
+//	0			0			0			0			0			0			1			0
 //	[rcv empty]	[trx empty]	[ready rcv]	[frame err]	[overrun]	[parityerr]	[doubleSpd]	[multiprocessor] 
 
-// UCSRnB - USART Control and Status Register n B
-// 	RXCIEn		TXCIEn		UDRIEn		RXENn		TXENn		UCSZn2		RXB8n		TXB8n
-//	0			0			1			0			0			0		
+// UCSR0B - USART Control and Status Register n B
+// 	RXCIE0		TXCIE0		UDRIE0		RXEN0		TXEN0		UCSZ02		RXB80		TXB80
+//	0			0			0			0			0			0			0			0
 //	[int on rx]	[int on tx]	[intonURDR]	[rcv enabl]	[trx enabl]	[char size]	[9 bit rcv]	[9th bit trx] 
 
-// UCSRnC - USART Control and Status Register n C
-// 	UMSELn1		UMSELn0		UPMn1		UPMn0		USBSn		UCSZn1		UCSZn0		UCPOLn
-//	0			0			1			0			0			0		
+// UCSR0C - USART Control and Status Register n C
+// 	UMSEL01		UMSEL00		UPM01		UPM00		USBS0		UCSZ01		UCSZ00		UCPOL0
+//	0			0			1			0			0			1			1			0	
 //	[set sync/async mode  ]	[ set parity mode     ]	[ stopbit ]	[set bits/frames      ]	[clock pol	]
 
-// UBRRnH - USART Baud Rate Register High
+// UBRR0H - USART Baud Rate Register High
 // 	-			-			-			-			UBRRn		UBRRn		UBRRn		UBRRn
 //	0			0			0			0			0			0			0			0
 //	[ Reserved									  ]	[ Baud rate high bits 							]
 
-// UBRRnL - USART Baud Rate Register Low
+// UBRR0L - USART Baud Rate Register Low
 // 	UBRRn		UBRRn		UBRRn		UBRRn		UBRRn		UBRRn		UBRRn		UBRRn
 //	0			0			0			0			0			0			0			0
 //	[ Baud rate low bits 																			]
 
 
+// int round()
+// {
+
+// }
+
+// calculate UBRRn register setting based on Table 20-1 p.182
+// ( F_CPU / (16 * BaudRate) ) - 1
+void	setBaudRate()
+{
+	unsigned int ubrr = ( F_CPU / (8 * BAUDRATE) ) - 1;
+	
+	UBRR0H = (unsigned char)(ubrr >> 8);
+	UBRR0L = (unsigned char) ubrr;
+}
+
 
 void uart_init()
 {
-	UBRR0H = (unsigned char)(UART_BAUDRATE >> 8);
-	UBRR0L = (unsigned char) UART_BAUDRATE;
-	UCSR0B = (1<<RXEN0) | (1<<TXEN0);
-	UCSR0C = (1<<USBS0) | (3<<UCSZ00);
+	setBaudRate();
+	
+	UCSR0A = (1 << U2X0);					// set DOUBLE SPEED to lessen rounding error
+	UCSR0B = (1 << RXEN0) | (1 << TXEN0); 	// enable recv and transmit
+	UCSR0C = (1 << USBS0) | (3 << UCSZ00);	// set stop bit and bit/frame
 }
 
-void uart_tx(char c)
+void uart_tx(unsigned char c)
 {
 	// Waiting for buffer to be empty
-	while ( !( UCSRnA & (1 << UDREn)) )
+	while ( !( UCSR0A & (1 << UDRE0)) )
 		;
 
 	// adding data
-	UDRn = c;
+	UDR0 = c;
 }
 
 
 
 int main()
 {
+	uart_init();
 
+	while (1)
+	{
+		uart_tx('Z');
+		_delay_ms(10);
+	}
 }
