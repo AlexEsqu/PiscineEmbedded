dc72d998f47dd05ce5e5e0e8093bfe39b6faf18c
Alex Kling <momiji.kling@gmail.com>
2026-04-24 10:02:45 +0200

    add m06ex00 frame

---
+++ Module06/ex00/main.c
@@ -0,0 +1,35 @@
+#include "libalex_avr.h"
+
+// • Le microcontrôleur AVR ATmega328P possède 1 interface I2C que vous devez uti-
+// liser dans cet exercice pour communiquer avec un capteur de température AHT20
+// (U3).
+// • Vous devez écrire une fonction i2c_init qui initialise l’I2C sur le microcontrôleur.
+// • L’I2C du MCU doit être configuré afin que la fréquence de communication soit de
+// 100 kHz.
+// • Vous devez écrire une fonction i2c_start qui démarre une transmission I2C entre
+// le microcontrôleur et le capteur.
+// • Le programme devra retourner sur votre ordinateur les valeurs de statut après
+// chaque envoi de donnée.
+// • Vous devez écrire une fonction i2c_stop qui interrompt la communication entre
+// le microcontrôleur et le capteur.
+
+void i2c_init(void)
+{
+
+}
+
+void i2c_start(void)
+{
+
+}
+
+void i2c_stop(void)
+{
+	
+}
+
+int main()
+{
+
+}
+
